5384ff8ca33fa7c95dcea975c6c937b139192e35
何波 <tbydmi@gmail.com>
2022-11-17 15:58:15 +0800

    同步

--- packages/components/check-box/src/check-box.vue
+++ packages/components/check-box/src/check-box.vue
@@ -12,16 +12,3 @@ export default {
   methods: {},
 };
 </script>
-[{ "resource":
-"/e:/gitHub/wdxx-ui/packages/components/checkbox/src/checkbox.vue", "owner":
-"_generated_diagnostic_collection_name_#4", "code": "1149", "severity": 8,
-"message": "File name
-'e:/gitHub/wdxx-ui/packages/components/checkbox/src/prop.ts' differs from
-already included file name
-'E:/gitHub/wdxx-ui/packages/components/checkbox/src/prop.ts' only in casing.\n
-The file is in the program because:\n Imported via './prop' from file
-'E:/gitHub/wdxx-ui/packages/components/checkbox/src/checkbox.vue'\n Imported via
-'./prop' from file
-'e:/gitHub/wdxx-ui/packages/components/checkbox/src/checkbox.vue'", "source":
-"Vetur", "startLineNumber": 6, "startColumn": 31, "endLineNumber": 6,
-"endColumn": 39 }]
