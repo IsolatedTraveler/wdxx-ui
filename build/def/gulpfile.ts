a282c74732b03d6cba6a5669c8bf3e9e1ef1330f
hb <tbydmi@gmail.com>
2022-11-21 22:25:00 +0800

    同步

--- build/def/gulpfile.ts
+++ build/def/gulpfile.ts
@@ -1,5 +1,11 @@
-import { series } from 'gulp'
-import {withTaskName, run} from '@ui/build-utils'
+import { series, parallel } from 'gulp'
+import { mkdir } from 'fs/promises'
+import {withTaskName, run, epOutput, runTask} from '@ui/build-utils'
 export default series(
-  withTaskName('clean', () => run('npx rimraf dist'))
+  withTaskName('clean', () => run('pnpm run clean')),
+  withTaskName('createOutput', () => mkdir(epOutput, { recursive: true })),
+  parallel(
+    runTask('buildModules')
+  )
 )
+export * from './tasks'
