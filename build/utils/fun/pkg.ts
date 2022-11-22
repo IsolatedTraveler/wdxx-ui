b034edab5334c6b9a5c827aa14fdbebbff3feccd
hb <tbydmi@gmail.com>
2022-11-22 09:58:34 +0800

    同步

--- build/utils/fun/pkg.ts
+++ build/utils/fun/pkg.ts
@@ -1,12 +1,35 @@
-import type { ProjectManifest } from '@pnpm/types'
+import type { ProjectManifest } from "@pnpm/types";
+import { PKG_NAME, PKG_PREFIX, buildConfig, Module } from "../var";
+
+export const excludeFiles = (files: string[]) => {
+  const excludes = ["node_modules", "test", "mock", "gulpfile", "dist"];
+  return files.filter(
+    (path) => !excludes.some((exclude) => path.includes(exclude))
+  );
+};
+
 export const getPackageManifest = (pkgPath: string) => {
-  return require(pkgPath) as ProjectManifest
-}
-export const getPackageDependencies  = (pkgPath: string): Record<'dependencies' | 'peerDependencies', string[]> => {
-  const manifest = getPackageManifest(pkgPath)
-  const { dependencies = {}, peerDependencies = {} } = manifest
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  return require(pkgPath) as ProjectManifest;
+};
+
+export const getPackageDependencies = (
+  pkgPath: string
+): Record<"dependencies" | "peerDependencies", string[]> => {
+  const manifest = getPackageManifest(pkgPath);
+  const { dependencies = {}, peerDependencies = {} } = manifest;
+
   return {
     dependencies: Object.keys(dependencies),
     peerDependencies: Object.keys(peerDependencies),
-  }
-}
+  };
+};
+export const pathRewriter = (module: Module) => {
+  const config = buildConfig[module];
+
+  return (id: string) => {
+    id = id.split(`${PKG_PREFIX}/theme`).join(`${PKG_NAME}/theme`);
+    id = id.split(`${PKG_PREFIX}/`).join(`${config.bundle.path}/`);
+    return id;
+  };
+};
--- example/src/main.ts
+++ example/src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import wdxx from '../../packages/z-ui/index'
+import wdxx from '../../dist/ui'
 const app = createApp(App)
 app.use(wdxx)
 app.mount('#app')
