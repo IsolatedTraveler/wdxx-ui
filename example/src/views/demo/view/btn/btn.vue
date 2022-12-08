7ceba5802538b0e38d9d0bc35f466d126aa85e87
何波 <tbydmi@gmail.com>
2022-12-08 14:31:33 +0800

    备份

--- example/src/views/demo/view/btn/btn.vue
+++ example/src/views/demo/view/btn/btn.vue
@@ -56,23 +56,28 @@
       <z-btn state="primary" radius="5px">5px</z-btn>
       <z-btn state="primary" radius="8">8</z-btn>
       <z-btn state="primary" radius="1em">1em</z-btn>
-      <z-btn state="primary" :radius="radius" style="width:6em">测试</z-btn>
+    </div>
+    <div>
+      <z-btn disabled state="">btn</z-btn>
+      <z-btn disabled state="primary">primary</z-btn>
+      <z-btn disabled state="success">success</z-btn>
+      <z-btn disabled state="warning">warning</z-btn>
+      <z-btn disabled state="danger">danger</z-btn>
+      <z-btn disabled state="info">info</z-btn>
+    </div>
+    <div>
+      <z-btn disabled shape="ghost" state="">ghost</z-btn>
+      <z-btn disabled shape="ghost" state="primary">primary</z-btn>
+      <z-btn disabled shape="ghost" state="success">success</z-btn>
+      <z-btn disabled shape="ghost" state="warning">warning</z-btn>
+      <z-btn disabled shape="ghost" state="danger">danger</z-btn>
+      <z-btn disabled shape="ghost" state="info">info</z-btn>
     </div>
   </div>
 </template>
 <script lang="ts">
 export default {
-  name: 'demo-btn',
-  data() {
-    return {
-      radius: 'circle'
-    }
-  },
-  mounted() {
-    setTimeout(() => {
-      this.radius = 'round'
-    }, 3000);
-  }
+  name: 'demo-btn'
 }
 </script>
 <style lang="scss">
