9aec34b4672f68feccda3dbe92fd5a1a55a96905
何波 <tbydmi@gmail.com>
2022-12-20 12:51:18 +0800

    同步use-table文件

--- packages/components/table/src/use-table.ts
+++ packages/components/table/src/use-table.ts
@@ -1,16 +1,18 @@
-import { useCss } from "@ui/hooks"
+import { useCss, useProvideTable } from "@ui/hooks"
 import { ref, SetupContext, computed } from "vue"
 import { TableEmits, TableProps } from "./table"
 export const useTable = (_props: TableProps, _emit: SetupContext<TableEmits>['emit']) => {
-  const _ref = ref<HTMLButtonElement>(), classVal = computed(() => ({
+  const _ref = ref<HTMLElement>(), _el_thead = ref<HTMLElement>(), classVal = computed(() => ({
     name: 'table'
-  })), {_class} = useCss(classVal, _ref), _main_class = '', _body_class = '', cols = ref([]), heads = ref([])
+  })), {_class} = useCss(classVal, _ref), _main_class = '', _body_class = '', {cols, ths, reload} = useProvideTable(_el_thead)
   return {
     _ref,
+    _el_thead,
     _class,
     _main_class,
     _body_class,
     cols,
-    heads
+    ths,
+    reload
   }
 }
