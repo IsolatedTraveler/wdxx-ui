2fe9dd84365b9086f30076c21b16a379e43881d8
何波 <tbydmi@gmail.com>
2022-12-20 12:56:20 +0800

    同步table文件

--- packages/components/table/src/table.vue
+++ packages/components/table/src/table.vue
@@ -1,5 +1,5 @@
 <template>
-  <div ref="_ref" :class="_class">
+  <div ref="_ref" :class="_class" :data-reload="reload">
     <!-- 获取最大固定高度 -->
     <div :class="_main_class">
       <!-- 获取table当前高度 -->
@@ -8,15 +8,26 @@
           <colgroup>
             <slot></slot>
           </colgroup>
-          <thead>
-            <tr v-for="(ths, i) in heads" :key="i">
-              <th v-for="(th,j) in ths" :key="j">
-                {{th}}
+          <thead ref="_el_thead">
+            <tr v-for="(tr, i) in ths" :key="i">
+              <th v-for="th in tr" :key="th.value.id" v-show="th.value.show" :id="th.value.id"
+                :style="(th.value.posStyle)"
+                :colspan="(th.value.colspan) || 1"
+                :rowspan="(th.value.rowspan) || 1"
+                 >
+                 <div :style="th.value.style">
+                  {{th.value.label}}
+                 </div>
               </th>
             </tr>
           </thead>
           <tbody>
-
+            <tr>
+              <td v-for="col in cols" :key="col.value.id" v-show="col.value.show"
+                :style="(col.value.posStyle)">
+                {{ col.value.name }}
+              </td>
+            </tr>
           </tbody>
         </table>
         <div></div>
@@ -34,9 +45,14 @@ defineOptions({
 })
 const props = defineProps(tableProps)
 const emit = defineEmits(tableEmits)
-const {_ref, _class, _main_class, _body_class, heads} = useTable(props, emit)
+const {_ref, _class, _main_class, _body_class, _el_thead, ths, cols, reload} = useTable(props, emit)
 const page = props.page ? {} : false
 defineExpose({
   ref: _ref
 })
 </script>
+<style lang="scss">
+  th,td{
+    border: 1px solid #dcdcdc;
+  }
+</style>
