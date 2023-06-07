cb257887fb5c6d88e7e1cc80eea3fae3efd58515
何波 <tbydmi@gmail.com>
2023-06-07 15:33:36 +0800

    Update index.ts

--- packages/hooks/index.ts
+++ packages/hooks/index.ts
@@ -1,4 +1,6 @@
-export * from './use-props'
 export * from './use-css'
+export * from './use-event'
 export * from './use-html'
+export * from './use-props'
 export * from './use-provide'
+export * from './use-util'
