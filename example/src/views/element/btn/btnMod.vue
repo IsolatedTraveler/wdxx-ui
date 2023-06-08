375ef83fa3ee48cff5a3765037c6689ac698b7e1
hb <tbydmi@gmail.com>
2023-06-08 08:08:05 +0800

    同步

--- example/src/views/element/btn/btnMod.vue
+++ example/src/views/element/btn/btnMod.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="element-btn-mod">
-    <z-btn :shape="shape" state="" full>{{ text }}</z-btn>
-    <z-btn :shape="shape" state="primary">主要按钮</z-btn>
-    <z-btn :shape="shape" state="success">成功按钮</z-btn>
-    <z-btn :shape="shape" state="warning">警告按钮</z-btn>
-    <z-btn :shape="shape" state="danger">危险按钮</z-btn>
-    <z-btn :shape="shape" state="info">信息按钮</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="">{{ text }}</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="primary">主要按钮</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="success">成功按钮</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="warning">警告按钮</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="danger">危险按钮</z-btn>
+    <z-btn :shape="shape" :size="size" :disabled="disabled" state="info">信息按钮</z-btn>
   </div>
 </template>
 <script lang="ts">
@@ -13,7 +13,9 @@ export default {
   name: 'element-btn-mod',
   props: {
     shape: String,
-    text: String
+    text: String,
+    size: String,
+    disabled: Boolean
   }
 }
 </script>
--- example/src/views/element/btn/index.vue
+++ example/src/views/element/btn/index.vue
@@ -3,30 +3,11 @@
     <element-btn-mod text="默认按钮"/>
     <element-btn-mod text="简洁按钮" shape="ghost"/>
     <element-btn-mod text="文字按钮" shape="text"/>
-    <div>
-      <z-btn size="sm" state="">sm</z-btn>
-      <z-btn size="sm" state="primary">primary</z-btn>
-      <z-btn size="sm" state="success">success</z-btn>
-      <z-btn size="sm" state="warning">warning</z-btn>
-      <z-btn size="sm" state="danger">danger</z-btn>
-      <z-btn size="sm" state="info">info</z-btn>
-    </div>
-    <div>
-      <z-btn size="md" state="">md</z-btn>
-      <z-btn size="md" state="primary">primary</z-btn>
-      <z-btn size="md" state="success">success</z-btn>
-      <z-btn size="md" state="warning">warning</z-btn>
-      <z-btn size="md" state="danger">danger</z-btn>
-      <z-btn size="md" state="info">info</z-btn>
-    </div>
-    <div>
-      <z-btn shape="ghost" size="lg" state="">lg</z-btn>
-      <z-btn shape="ghost" size="lg" state="primary">primary</z-btn>
-      <z-btn shape="ghost" size="lg" state="success">success</z-btn>
-      <z-btn shape="ghost" size="lg" state="warning">warning</z-btn>
-      <z-btn shape="ghost" size="lg" state="danger">danger</z-btn>
-      <z-btn shape="ghost" size="lg" state="info">info</z-btn>
-    </div>
+    <element-btn-mod text="禁用按钮" disabled/>
+    <element-btn-mod text="禁用按钮" shape="ghost" disabled/>
+    <element-btn-mod text="禁用按钮" shape="text" disabled/>
+    <element-btn-mod text="小型按钮" size="sm"/>
+    <element-btn-mod text="大型按钮" size="lg"/>
     <div>
       <z-btn state="primary" radius="round">round</z-btn>
       <z-btn state="primary" radius="circle">circle</z-btn>
@@ -35,22 +16,6 @@
       <z-btn state="primary" radius="5px">5px</z-btn>
       <z-btn state="primary" radius="8">8</z-btn>
       <z-btn state="primary" radius="1em">1em</z-btn>
-    </div>
-    <div>
-      <z-btn disabled state="">btn</z-btn>
-      <z-btn disabled state="primary">primary</z-btn>
-      <z-btn disabled state="success">success</z-btn>
-      <z-btn disabled state="warning">warning</z-btn>
-      <z-btn disabled state="danger">danger</z-btn>
-      <z-btn disabled state="info">info</z-btn>
-    </div>
-    <div>
-      <z-btn disabled shape="ghost" state="">ghost</z-btn>
-      <z-btn disabled shape="ghost" state="primary">primary</z-btn>
-      <z-btn disabled shape="ghost" state="success">success</z-btn>
-      <z-btn disabled shape="ghost" state="warning">warning</z-btn>
-      <z-btn disabled shape="ghost" state="danger">danger</z-btn>
-      <z-btn disabled shape="ghost" state="info">info</z-btn>
     </div>
   </div>
 </template>
