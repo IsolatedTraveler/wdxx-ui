c96b8b276d303ec0f8457f80bd6d730aa718c798
何波 <tbydmi@gmail.com>
2023-06-26 17:50:39 +0800

    Update component.ts

--- build/utils/var/component.ts
+++ build/utils/var/component.ts
@@ -21,7 +21,26 @@ export interface filesObj {
 const comArrObj: any = {}
 const comObj: ComsObj = require(path.resolve(projRoot, 'component.json'))
 const comKey: string[] = Object.keys(comObj)
-comObj.keys = []
+const keys: string[] = []
+comKey.map(key => {
+  let obj = comObj[key] as ComObj | true
+  if (obj === true) {
+    obj = { keys: [key] } as ComObj
+  } else {
+    obj.keys = []
+    if (obj.prev) {
+      obj.keys.push(...obj.prev)
+    }
+    obj.keys.push(key)
+    if (obj.next) {
+      obj.keys.push(...obj.next)
+    }
+  }
+  comObj[key] = obj
+  keys.push(...obj.keys)
+})
+comObj.keys = keys
+console.log(JSON.stringify(comObj))
 export {
   comObj,
   comKey,
