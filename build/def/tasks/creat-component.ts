de8aff944fc63471d9d3828cb25903fb055a1156
何波 <tbydmi@gmail.com>
2023-06-26 17:56:03 +0800

    Update creat-component.ts

--- build/def/tasks/creat-component.ts
+++ build/def/tasks/creat-component.ts
@@ -38,42 +38,43 @@ async function UiComponent() {
   ])
 }
 function createCom(arr: Array<filesObj>) {
-  excludeCom(arr)
+  // excludeCom(arr)
   // const keys = Object.keys(comObj)
-  // return Promise.all(keys.map(async (key) => {
-  //   const keyComUrl = resolve(compRoot, key), styleUrl = resolve(keyComUrl, 'style'), styleMod = resolve(stylesModuleRoot, key), arr: Array<Promise<any>> = []
-  //   await mkdir(keyComUrl, { recursive: true })
-  //   await mkdir(styleUrl, { recursive: true })
-  //   await mkdir(styleMod, { recursive: true })
-  //   if (comObj[key] === true) {
-  //     const comUrl = resolve(keyComUrl, 'src')
-  //     // 创建components
-  //     await mkdir(comUrl, { recursive: true })
-  //     // 创建components/${key}/src/${key}.ts
-  //     arr.push(write(resolve(comUrl, key + '.ts'), componentProp(key)))
-  //     // 创建components/${key}/src/${key}.vue
-  //     arr.push(write(resolve(comUrl, key + '.vue'), componentVue(key)))
-  //     // 创建components/${key}/src/insstance.ts
-  //     arr.push(write(resolve(comUrl, 'instance.ts'), componentInstance(key, comArrObj[key])))
-  //     // 创建components/${key}/src/use-${key}.ts
-  //     arr.push(write(resolve(comUrl, 'use-' + key + '.ts'), componentUse(key)))
-  //     // // 创建components/${key}/index.ts
-  //     arr.push(write(resolve(keyComUrl, 'index.ts'), componentIndex(key, comArrObj[key])))
-  //     delete comObj[key]
-  //   }
-  //   // 创建hooks/use-provide/${key}.ts
-  //   // console.log(resolve(hooksRoot, `${key}.ts`))
-  //   // arr.push(write(resolve(hooksRoot, `${key}.ts`), getHooks(key)))
-  //   // 创建components/${key}/style/index.ts
-  //   arr.push(write(resolve(styleUrl, 'index.ts'), `import '@ui/styles/src/base.scss'\nimport '@ui/styles/src/${key}.scss'\nimport '@ui/styles/src/end.scss'`))
-  //   // 创建components/${key}/style/css.ts
-  //   arr.push(write(resolve(styleUrl, 'css.ts'), `import '@ui/styles/base.css'\nimport '@ui/styles/${key}.css'\nimport '@ui/styles/end.css'`))
-  //   // 创建styles/src/mod/${key}/index.scss
-  //   arr.push(write(resolve(styleMod, 'index.scss'), `@use 'sass:map';\n@use '../../mixins/index.scss' as *;\n@use '../../config/index.scss' as *;\n@use './${key}.scss' as *;\n@include styles(${key}, $${key}, $attr, $state, $media);\n@include create(${key}) {\n\n}`))
-  //   // 创建styles/src/mod/${key}/${key}.scss
-  //   arr.push(write(resolve(styleMod, key + '.scss'), `@use 'sass:map';\n@use '../../config/index.scss' as *;\n$attr:('disabled');\n$media: ('hover');\n$state: ();\n$${key}: (\n  'mod': (),\n  'attr': setAttr($attr),\n  'state': setState($state),\n  'media': setMedia($media)\n) !default;`))
-  //   return Promise.all(arr)
-  // }))
+  const keys = ["icon"]
+  return Promise.all(keys.map(async (key) => {
+    const keyComUrl = resolve(compRoot, key), styleUrl = resolve(keyComUrl, 'style'), styleMod = resolve(stylesModuleRoot, key), arr: Array<Promise<any>> = []
+    await mkdir(keyComUrl, { recursive: true })
+    await mkdir(styleUrl, { recursive: true })
+    await mkdir(styleMod, { recursive: true })
+    //   if (comObj[key] === true) {
+    //     const comUrl = resolve(keyComUrl, 'src')
+    //     // 创建components
+    //     await mkdir(comUrl, { recursive: true })
+    //     // 创建components/${key}/src/${key}.ts
+    //     arr.push(write(resolve(comUrl, key + '.ts'), componentProp(key)))
+    //     // 创建components/${key}/src/${key}.vue
+    //     arr.push(write(resolve(comUrl, key + '.vue'), componentVue(key)))
+    //     // 创建components/${key}/src/insstance.ts
+    //     arr.push(write(resolve(comUrl, 'instance.ts'), componentInstance(key, comArrObj[key])))
+    //     // 创建components/${key}/src/use-${key}.ts
+    //     arr.push(write(resolve(comUrl, 'use-' + key + '.ts'), componentUse(key)))
+    //     // // 创建components/${key}/index.ts
+    //     arr.push(write(resolve(keyComUrl, 'index.ts'), componentIndex(key, comArrObj[key])))
+    //     delete comObj[key]
+    //   }
+    //   // 创建hooks/use-provide/${key}.ts
+    //   // console.log(resolve(hooksRoot, `${key}.ts`))
+    //   // arr.push(write(resolve(hooksRoot, `${key}.ts`), getHooks(key)))
+    //   // 创建components/${key}/style/index.ts
+    //   arr.push(write(resolve(styleUrl, 'index.ts'), `import '@ui/styles/src/base.scss'\nimport '@ui/styles/src/${key}.scss'\nimport '@ui/styles/src/end.scss'`))
+    //   // 创建components/${key}/style/css.ts
+    //   arr.push(write(resolve(styleUrl, 'css.ts'), `import '@ui/styles/base.css'\nimport '@ui/styles/${key}.css'\nimport '@ui/styles/end.css'`))
+    //   // 创建styles/src/mod/${key}/index.scss
+    //   arr.push(write(resolve(styleMod, 'index.scss'), `@use 'sass:map';\n@use '../../mixins/index.scss' as *;\n@use '../../config/index.scss' as *;\n@use './${key}.scss' as *;\n@include styles(${key}, $${key}, $attr, $state, $media);\n@include create(${key}) {\n\n}`))
+    //   // 创建styles/src/mod/${key}/${key}.scss
+    //   arr.push(write(resolve(styleMod, key + '.scss'), `@use 'sass:map';\n@use '../../config/index.scss' as *;\n$attr:('disabled');\n$media: ('hover');\n$state: ();\n$${key}: (\n  'mod': (),\n  'attr': setAttr($attr),\n  'state': setState($state),\n  'media': setMedia($media)\n) !default;`))
+    //   return Promise.all(arr)
+  }))
 }
 function excludeCom(arr: Array<filesObj>) {
   arr && arr.forEach(({ fileName }) => {
