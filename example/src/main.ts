13f942cdb6c3b04f859c9630c0d63571fceadbc6
何波 <tbydmi@gmail.com>
2023-06-28 14:03:30 +0800

    同步

--- example/src/main.ts
+++ example/src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import wdxx from '../../packages/z-ui/index'
-import '../../packages/styles/src/index.scss';
+import '../../packages/styles/dist/index.css';
 const app = createApp(App)
 app.use(wdxx)
 app.use(router)
