aa734855469e203797828b92f69d3e45295695f7
何波 <tbydmi@gmail.com>
2023-07-07 17:50:35 +0800

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,7 @@ declare module "@vue/runtime-core" {
     ZBtn: typeof import("z-uis")["ZBtn"];
     ZBtnGroup: typeof import("z-uis")["ZBtnGroup"];
     ZForm: typeof import("z-uis")["ZForm"];
+    ZCallKit: typeof import("z-uis")["ZCallKit"];
   }
   interface ComponentCustomProperties {
     
