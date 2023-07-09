1ef7b4872b83497d67ef82b85a76418f7d918a01
何波 <tbydmi@gmail.com>
2023-07-09 11:52:20 +0800

    Update index.ts

--- example/src/router/def/index.ts
+++ example/src/router/def/index.ts
@@ -1,5 +1,6 @@
 import base from './base'
 import element from './element'
+import component from './component'
 export default [
   {
     path: '/base',
@@ -11,5 +12,10 @@ export default [
     redirect: '/element/btn',
     title: '页面元素',
     children: element
+  }, {
+    path: '/component',
+    redirect: '/component/callKit',
+    title: '组件元素',
+    children: component
   }
 ]
