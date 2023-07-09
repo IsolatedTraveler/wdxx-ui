7df1b756edf59f593e91e311d79019752bf6acc6
何波 <tbydmi@gmail.com>
2023-07-09 12:02:10 +0800

    Create index.vue

---
+++ example/src/views/component/callKit/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="component-callKit">
+    <z-call-kit />
+  </div>
+</template>
+<script>
+export default {
+  name: 'component-callKit',
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss">
+.component-callKit {}
+</style>
