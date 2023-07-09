e4d89269aa8bb3baa21ab7a6ed3cb82a1872cf65
何波 <tbydmi@gmail.com>
2023-07-09 14:39:27 +0800

    Update flex-child.ts

--- packages/hooks/use-inject/flex-child.ts
+++ packages/hooks/use-inject/flex-child.ts
@@ -1,7 +1,7 @@
-import { FlexAutoProps } from "@ui/components/flex/src/flex-child";
+import { FlexChildProps } from "@ui/components/flex/src/flex-child";
 import { inject } from "vue";
 import { provideFlexId } from "../use-provide/flex";
-export const useInjectFlexAuto = (props: FlexAutoProps) => {
+export const useInjectFlexAuto = (props: FlexChildProps) => {
   const { } = inject(provideFlexId, {})
   return {
   }
