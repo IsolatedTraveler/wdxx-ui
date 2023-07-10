b72746a4b2dcb53a88a8abcb108ebe807ddb67db
何波 <tbydmi@gmail.com>
2023-07-10 15:12:16 +0800

    Update index.vue

--- example/src/views/element/flex/index.vue
+++ example/src/views/element/flex/index.vue
@@ -1,19 +1,19 @@
 <template>
   <z-flex col class="element-flex">
     <z-flex justify="end" align="end" self="end" wrap>
-      <z-flex>1</z-flex>
-      <z-flex>2</z-flex>
-      <z-flex>3</z-flex>
+      <z-flex auto="1">1</z-flex>
+      <z-flex auto="2">2</z-flex>
+      <z-flex auto="3">3</z-flex>
     </z-flex>
     <z-flex>
-      <z-flex>4</z-flex>
-      <z-flex>5</z-flex>
-      <z-flex>6</z-flex>
+      <z-flex auto="1">4</z-flex>
+      <z-flex auto="1">5</z-flex>
+      <z-flex auto="1">6</z-flex>
     </z-flex>
     <z-flex>
-      <z-flex>7</z-flex>
-      <z-flex>8</z-flex>
-      <z-flex>9</z-flex>
+      <z-flex auto="1">7</z-flex>
+      <z-flex auto="2">8</z-flex>
+      <z-flex auto="2">9</z-flex>
     </z-flex>
   </z-flex>
 </template>
