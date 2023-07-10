b10fc26496e88d69b8189912070d684158ee301a
何波 <tbydmi@gmail.com>
2023-07-10 14:19:04 +0800

    Update global.d.ts

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,9 @@
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
+    ZFlex: typeof import("z-uis")["ZFlex"];
     ZIcon: typeof import("z-uis")["ZIcon"];
     ZBtn: typeof import("z-uis")["ZBtn"];
     ZBtnGroup: typeof import("z-uis")["ZBtnGroup"];
-    ZFlex: typeof import("z-uis")["ZFlex"];
-    ZFlexAuto: typeof import("z-uis")["ZFlexAuto"];
     ZForm: typeof import("z-uis")["ZForm"];
     ZCallKit: typeof import("z-uis")["ZCallKit"];
   }
