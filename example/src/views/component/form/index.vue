ec96b1143f0e3751a37e9fd8fa6da75d257e9995
hb <tbydmi@gmail.com>
2023-07-10 22:55:35 +0800

    Create index.vue

---
+++ example/src/views/component/form/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="component-form">
+  </div>
+</template>
+<script>
+export default {
+  name: 'component-form'
+}
+</script>
+<style lang="scss">
+.component-form{
+}
+</style>
