84830e4d5a17be31f5171498c09321d671add030
何波 <tbydmi@gmail.com>
2023-07-11 18:27:06 +0800

    Update flex.ts

--- packages/vars/props/flex.ts
+++ packages/vars/props/flex.ts
@@ -1,4 +1,8 @@
 export const propsFlex = ['', 'row', 'col']
-export const propsFlexJustify = ['', 'start', 'end', 'center', 'stretch', 'around', 'between', 'evenly']
+export type PropsFlexV = '' | 'row' | 'col'
 export const propsFlexAlign = ['', 'start', 'end', 'center', 'stretch', 'around', 'between']
+export type PropsFlexAlignV = '' | 'start' | 'end' | 'center' | 'stretch' | 'around' | 'between'
+export const propsFlexJustify = ['', 'start', 'end', 'center', 'stretch', 'around', 'between', 'evenly']
+export type PropsFlexJustifyV = '' | 'start' | 'end' | 'center' | 'stretch' | 'around' | 'between' | 'evenly'
 export const propsFlexSelf = ['', 'start', 'end', 'center', 'stretch']
+export type PropsFlexSelfV = '' | 'start' | 'end' | 'center' | 'stretch'
