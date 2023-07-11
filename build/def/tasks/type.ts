4b871030aa9ebadc8250ddcb9020b336aa317247
何波 <tbydmi@gmail.com>
2023-07-11 18:30:12 +0800

    Update type.ts

--- build/def/tasks/type.ts
+++ build/def/tasks/type.ts
@@ -3,21 +3,22 @@ import { ObjStr } from "@ui/vars"
 import path from "path"
 const vars = require(path.resolve(projRoot, 'var.json'))
 const ml = vars.ml
-function getPage(ml: string) {
-  var page = vars[ml]
+function getPage(ml: string, page: Array<string>) {
   return readFile(path.resolve(varsRoot, ml)).then((files) => {
     if (page && page.length) {
       return (files as Array<ObjStr>).filter(({ name }) => {
-        name = name.split('.')[0]
-        return !page.includes(name)
+        var key = name.split('.')[0]
+        return !page.includes(key) || !page.includes(name)
       })
     }
     return files as Array<ObjStr>
   })
 }
 function create(ml: string) {
-  return getPage(ml).then((files) => {
-    return Promise.all(files.map(({ file }) => {
+  var page = vars[ml]
+  return getPage(ml, page).then((files) => {
+    return Promise.all(files.map(({ file, name }) => {
+      page.push(name)
       const data = require(file)
       var keys = Object.keys(data)
       keys = keys.map(key => {
