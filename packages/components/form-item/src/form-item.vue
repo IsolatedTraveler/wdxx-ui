153cba6de7d4f06bc469415ee9c122ea0abcf72a
hb <tbydmi@gmail.com>
2023-07-21 08:43:51 +0800

    Update form-item.vue

--- packages/components/form-item/src/form-item.vue
+++ packages/components/form-item/src/form-item.vue
@@ -1,10 +1,11 @@
 <template>
-  <div ref="_ref" :class="_class">
+  <z-flex ref="_ref" :class="_class">
     <label>{{ label }}</label>
     <slot></slot>
-  </div>
+  </z-flex>
 </template>
 <script lang="ts" setup>
+import { ZFlex } from '../../flex';
 import { formItemEmits, formItemProps } from './form-item'
 import { useFormItem } from './use-form-item'
 defineOptions({
