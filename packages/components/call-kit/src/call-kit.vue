a5a225e932d09cacf4a8b0eb4f58ea37881dad63
何波 <tbydmi@gmail.com>
2023-07-23 19:28:50 +0800

    Update call-kit.vue

--- packages/components/call-kit/src/call-kit.vue
+++ packages/components/call-kit/src/call-kit.vue
@@ -1,11 +1,13 @@
 <template>
   <z-flex ref="_ref" :class="_class">
-    <z-media class="z-call-kit--main" v-bind="main"></z-media>
-    <z-media v-if="single" v-show="singleMedia" class="z-call-kit--remote" v-bind="singleMedia" @click.stop="clickId = singleMedia.userId"></z-media>
+    <z-media :class="{ 'z-call-kit--remote': _mainClassJudge, 'z-call-kit--main': !_mainClassJudge }" v-bind="main"
+      :key="main?.userId" @click.stop="click(main?.userId)"></z-media>
+    <z-media v-if="single" v-show="singleMedia"
+      :class="{ 'z-call-kit--remote': !_mainClassJudge, 'z-call-kit--main': _mainClassJudge }" v-bind="singleMedia"
+      @click.stop="click(singleMedia?.userId)"></z-media>
     <z-flex class="z-call-kit--multi" wrap ref="_refMulti" v-else>
-      <z-media v-for="(it) in medias" :key="it.userId" :auto="multiWidth"
-        :order="it.userId == localId ? 1 : it.userId == clickId ? 2 : 3" v-bind="it"
-        @click.stop="clickId = it.userId"></z-media>
+      <z-media v-for="(it) in medias" :key="it.userId" :auto="multiWidth" :order="it.userId == localId ? 1 : 2"
+        v-bind="it" @click.stop="click(it.userId)"></z-media>
     </z-flex>
   </z-flex>
 </template>
@@ -19,7 +21,15 @@ defineOptions({
 })
 const props = defineProps(callKitProps)
 const emit = defineEmits(callKitEmits)
-const { _ref, _class, main, clickId, singleMedia, medias, _refMulti, multiWidth } = useCallKit(props, emit)
+function click(id: string = '') {
+  if (props.single) {
+    clickId.value = id
+  } else if (props.mainId == props.localId && props.mainId != id) {
+    // 提示是否切换主讲人权限
+    // 切换主讲人
+  }
+}
+const { _ref, _class, main, clickId, singleMedia, medias, _refMulti, multiWidth, _mainClassJudge } = useCallKit(props, emit)
 defineExpose({
   ref: _ref
 })
