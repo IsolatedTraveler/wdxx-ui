b65a93d7ceb2c51022e55c318cca5d57f6d761be
何波 <tbydmi@gmail.com>
2023-08-04 10:39:43 +0800

    Update creat-component.ts

--- build/def/tasks/creat-component.ts
+++ build/def/tasks/creat-component.ts
@@ -111,6 +111,7 @@ function createCom(arr: Array<filesObj>) {
       mkdir(keyComUrl, { recursive: true }),
       mkdir(comUrl, { recursive: true })
     ]).then(() => {
+      console.log(obj)
       return Promise.all(obj.keys.map(it => creatComponentMod(it, comUrl)))
     }).then(() => {
       const arr: Array<Promise<any>> = []
