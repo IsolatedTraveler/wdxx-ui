982124934d1abb065d1b834d389bef6d1bed6bf3
何波 <tbydmi@gmail.com>
2023-08-09 09:23:50 +0800

    Create index.vue

---
+++ example/src/views/component/tree/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="component-tree">
+  </div>
+</template>
+<script>
+export default {
+  name: 'component-tree',
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss">
+.component-tree {}
+</style>
