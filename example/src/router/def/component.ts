27355b98b913c6d843c84da76a46b29b0f19e0fa
何波 <tbydmi@gmail.com>
2023-08-09 09:23:47 +0800

    Update component.ts

--- example/src/router/def/component.ts
+++ example/src/router/def/component.ts
@@ -2,5 +2,6 @@ import { getRoute } from './getRoute';
 let ml = 'component'
 export default [
   getRoute(ml, 'callKit', '视频通话'),
-  getRoute(ml, 'form', '表单')
+  getRoute(ml, 'form', '表单'),
+  getRoute(ml, 'tree', '树')
 ]
