bc6641be1ca2d229f3b8da6c2b4198a9e499512a
何波 <tbydmi@gmail.com>
2023-08-10 14:34:08 +0800

    Update use-media.ts

--- packages/components/media/src/use-media.ts
+++ packages/components/media/src/use-media.ts
@@ -1,40 +1,18 @@
 import { useCss } from "@ui/hooks"
 import { ref, SetupContext, computed, watch, nextTick } from "vue"
 import { MediaEmits, MediaProps } from "./media"
+import { setHeightByWidth } from "@ui/utils/css"
 export const useMedia = (props: MediaProps, emit: SetupContext<MediaEmits>['emit']) => {
   const _ref = ref<HTMLDivElement>(), classVal = computed(() => ({
     name: 'media'
   })), { _class, _style } = useCss(classVal, _ref)
     , calc = document.body.clientHeight / document.body.clientWidth
-  var maxWidth: number, marginLeft: number
-  function setHeight(width: number) {
-    var el = _ref.value
-    if (el) {
-      var elWidth = el.clientWidth
-      if (!maxWidth) {
-        var obj = getComputedStyle(el)
-        maxWidth = parseFloat(obj.maxWidth)
-        marginLeft = parseFloat(obj.marginLeft)
-      }
-      if (width > maxWidth) {
-        elWidth = (width / Math.ceil(width / maxWidth)) - marginLeft - 1
-        _style.value.width = (elWidth / 100) + 'rem'
-      }
-      _style.value.height = (calc * elWidth / 100) + 'rem'
-    } else {
-      nextTick(() => {
-        setHeight(width)
-      })
-    }
-  }
+    , variable = { maxWidth: 0, marginLeft: 0 }
+  // 设置高度
   watch(() => props.auto, (v) => {
-    if (v) {
-      setHeight(v)
-    } else {
-      _style.value.width = undefined
-      _style.value.height = undefined
-    }
+    setHeightByWidth(v, _style.value, _ref, calc, variable)
   }, { immediate: true })
+  // 播放视频
   watch(() => props.video, (v, o) => {
     if (v && !o) {
       if (_ref.value) {
@@ -44,6 +22,12 @@ export const useMedia = (props: MediaProps, emit: SetupContext<MediaEmits>['emit
           v.play(_ref.value)
         })
       }
+    }
+  }, { immediate: true })
+  // 播放音频
+  watch(() => props.isAudio ? props.audio : undefined, (v, o) => {
+    if (v && !o) {
+      v.play()
     }
   }, { immediate: true })
   return {
