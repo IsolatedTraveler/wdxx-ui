0e31bf167660f60ed82fab8fd9eb82a82df02446
何波 <tbydmi@gmail.com>
2023-08-10 11:33:10 +0800

    Update component.ts

--- packages/z-ui/component.ts
+++ packages/z-ui/component.ts
@@ -1,15 +1,15 @@
-import {ZFlex} from '@ui/components/flex'
-import {ZIcon} from '@ui/components/icon'
-import {ZMedia} from '@ui/components/media'
-import {ZBtn,ZBtnGroup} from '@ui/components/btn'
-import {ZCallKit} from '@ui/components/call-kit'
-import {ZForm} from '@ui/components/form'
-import {ZFormItems} from '@ui/components/form-items'
-import {ZFormItem} from '@ui/components/form-item'
-import {ZInput} from '@ui/components/input'
-import {ZSelect} from '@ui/components/select'
-import {ZTreeItem,ZTree} from '@ui/components/tree'
-export default[
+import { ZFlex } from '@ui/components/flex'
+import { ZIcon } from '@ui/components/icon'
+import { ZMedia } from '@ui/components/media'
+import { ZBtn, ZBtnGroup } from '@ui/components/btn'
+import { ZCallKit } from '@ui/components/call-kit'
+import { ZForm } from '@ui/components/form'
+import { ZFormItems } from '@ui/components/form-items'
+import { ZFormItem } from '@ui/components/form-item'
+import { ZInput } from '@ui/components/input'
+import { ZSelect } from '@ui/components/select'
+import { ZTreeItem, ZTree } from '@ui/components/tree'
+export default [
   ZFlex,
   ZIcon,
   ZMedia,
