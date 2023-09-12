14d9158414a5fc788283fe583f3ee4e76e032138
何波 <tbydmi@gmail.com>
2023-09-12 16:40:40 +0800

    Update use-tree.ts

--- packages/components/tree/src/use-tree.ts
+++ packages/components/tree/src/use-tree.ts
@@ -1,10 +1,11 @@
-import { useCss } from "@ui/hooks"
+import { useCss, useProvideTree } from "@ui/hooks"
 import { ref, SetupContext, computed } from "vue"
 import { TreeEmits, TreeProps } from "./tree"
 export const useTree = (props: TreeProps, emit: SetupContext<TreeEmits>['emit']) => {
   const _ref = ref<HTMLButtonElement>(), classVal = computed(() => ({
     name: 'tree'
   })), { _class } = useCss(classVal, _ref)
+  useProvideTree(props)
   return {
     _ref,
     _class
