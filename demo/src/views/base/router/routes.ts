ee06627b6c14b6cac102e7fe148d33ad9ccc49dd
何波 <tbydmi@gmail.com>
2023-09-27 17:52:40 +0800

    Update routes.ts

--- demo/src/views/base/router/routes.ts
+++ demo/src/views/base/router/routes.ts
@@ -3,14 +3,29 @@ interface Routes {
   [key: string]: RouteRecordRaw
 }
 export default {
-  baseLogin: {
-    name: 'baseLogin',
-    path: '/baseLogin',
-    component: () => import('@base/login/index.vue')
+  base404: {
+    name: 'base404',
+    path: '/base404',
+    component: () => import('@base/404/index.vue')
   },
   baseHome: {
     name: 'baseHome',
     path: '/baseHome',
     component: () => import('@base/home/index.vue')
+  },
+  baseLoad: {
+    name: 'baseLoad',
+    path: '/baseLoad',
+    component: () => import('@base/load/index.vue')
+  },
+  baseLogin: {
+    name: 'baseLogin',
+    path: '/baseLogin',
+    component: () => import('@base/login/index.vue')
+  },
+  baseMenu: {
+    name: 'baseMenu',
+    path: '/baseMenu',
+    component: () => import('@base/menu/index.vue')
   }
 } as Routes
