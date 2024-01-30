132684ffe0e771431bac19ae6460922ec6276807
何波 <tbydmi@gmail.com>
2024-01-30 19:18:59 +0800

    同步

--- demo/config/plugin.ts
+++ demo/config/plugin.ts
@@ -1,3 +1,19 @@
 import vue from '@vitejs/plugin-vue'
-import { PluginOption } from 'vite'
-export const plugins: PluginOption[] = [vue()]
+import Inspect from 'vite-plugin-inspect'
+import esbuild from 'rollup-plugin-esbuild'
+import { PluginOption, Plugin } from 'vite'
+const esbuildPlugin = (): Plugin => ({
+  ...esbuild({
+    target: 'chrome64',
+    include: /.vue/,
+    loaders: {
+      '.vue': 'js',
+    },
+  }),
+  enforce: 'post'
+})
+export const plugins: PluginOption[] = [
+  vue(),
+  esbuildPlugin(),
+  Inspect()
+]
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,7 +3,7 @@ import { build, plugins, server, css, resolve } from './config'
 export default defineConfig({
   base: './',
   optimizeDeps: {
-    include: []
+    include: ['vue', '@vue/shared']
   },
   build,
   resolve,
