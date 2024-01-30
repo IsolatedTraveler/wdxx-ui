f733a17aee2b4c42eea818741f1255a2742b5562
何波 <tbydmi@gmail.com>
2024-01-30 18:49:15 +0800

    同步

--- viteDemo/src/main.ts
+++ viteDemo/src/main.ts
@@ -4,12 +4,15 @@ import App from './App.vue'
 import { router, loadXtxx } from "./router";
 import * as api from './api'
 import * as util from '@assets/index'
+import zUi from 'z-uis/index'
+import '@ui/styles/dist/index.css'
 const app = createApp(App)
 util.setFontSize(1200)
 app.config.globalProperties.$api = api;
 app.config.globalProperties.$util = util;
 app.use(createPinia())
+app.use(zUi)
 loadXtxx('base').then(() => {
   app.use(router as any)
-  app.mount(document.body)
+  app.mount('#app')
 })
