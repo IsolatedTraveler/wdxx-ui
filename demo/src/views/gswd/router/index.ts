3089fa57058f45241b53d11210efc083b33b29f3
何波 <tbydmi@gmail.com>
2024-02-21 15:43:22 +0800

    Update index.ts

--- demo/src/views/gswd/router/index.ts
+++ demo/src/views/gswd/router/index.ts
@@ -1,12 +1,12 @@
 import routes from './routes'
-import * as fbdq from './fbdq'
+import * as fbdq from './fbdq/index'
 import { getConfig } from '@/api'
 import { RouteRecordRaw } from 'vue-router'
 import { useLoadStore } from '@/store'
 export default function () {
   return getConfig('fbdq').then(v => {
     const root = useLoadStore().getRoot
-    return Object.values({ ...routes, ...(fbdq[v] || {}) }).map((it) => {
+    return Object.values({ ...routes, ...((fbdq as any)[v] || {}) }).map((it) => {
       (it as RouteRecordRaw).path = (root + (it as RouteRecordRaw).path).replace(/\/\//g, '/')
       return it
     }) as RouteRecordRaw[]
