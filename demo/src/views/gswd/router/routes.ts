95077b95fbb724e751fb0a98036a0626a1451fe1
何波 <tbydmi@gmail.com>
2024-02-21 15:45:03 +0800

    Update routes.ts

--- demo/src/views/gswd/router/routes.ts
+++ demo/src/views/gswd/router/routes.ts
@@ -1,10 +1,5 @@
 import { Routes, getRoute } from "@/router"
 const routes: Routes = {}
-export const xtm = 'qtxt'
-getRoute(routes, xtm, '404')
-getRoute(routes, xtm, 'home')
-getRoute(routes, xtm, 'load')
-getRoute(routes, xtm, 'login')
-getRoute(routes, xtm, 'menu')
-getRoute(routes, xtm, 'dmgl')
+export const xtm = 'gswd'
+getRoute(routes, xtm, 'publish')
 export default routes
