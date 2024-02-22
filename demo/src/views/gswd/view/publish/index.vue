ba171674a063fadd7af5d0598b1d527a926662d5
何波 <tbydmi@gmail.com>
2024-02-22 19:02:47 +0800

    同步

--- demo/src/views/gswd/view/publish/index.vue
+++ demo/src/views/gswd/view/publish/index.vue
@@ -1,12 +1,14 @@
 <template>
   <z-flex flex="row" class="use-publish">
     <z-tree class="left" :data="menu" def="java"></z-tree>
-    <z-flex class="content"></z-flex>
+    <z-flex class="content">
+      <Java></Java>
+    </z-flex>
   </z-flex>
 </template>
 <script lang="ts" setup>
 import { usePublish } from './use-publish';
-
+import Java from './module/java.vue'
 defineOptions({
   name: 'gswd-publish'
 })
---
+++ demo/src/views/gswd/view/publish/module/java.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <pre>
+      <code class="java">
+        cd /usr/local/src/
+        tar -zxvf jdk-8u333-linux-x64.tar.gz 
+        mv jdk1.8.0_333 /usr/lib/jvm/
+        cd /etc/
+        vi /etc/profile
+      </code>
+    </pre>
+    拷贝以下代码到当前打开文件末尾
+    <pre>
+      <code>
+        export JAVA_HOME=/usr/lib/jvm/jdk1.8.0_333
+        export JRE_HOME=$JAVA_HOME/jre
+        export PATH=$PATH:$JAVA_HOME/bin
+        export CLASSPATH=.:$JAVA_HOME/lib:$JRE_HOME/lib
+      </code>
+    </pre>
+    <pre>
+      <code>
+        source /etc/profile
+        java -version
+        javac -version
+      </code>
+    </pre>
+  </div>
+</template>
+<script>
+export default {
+  name: 'java',
+  data() {
+    return {}
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="scss">
+// .java {}
+</style>
