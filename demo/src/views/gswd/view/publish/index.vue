88bf02ab6689f93c0259c1a9fb64107b5d379516
何波 <tbydmi@gmail.com>
2024-02-22 17:10:14 +0800

    Update index.vue

--- demo/src/views/gswd/view/publish/index.vue
+++ demo/src/views/gswd/view/publish/index.vue
@@ -1,5 +1,7 @@
 <template>
-  <div style="text-align: center;font-size: .56rem;line-height: 2;">公司文档发版流程</div>
+  <z-flex flex="row">
+    <div style="text-align: center;font-size: .56rem;line-height: 2;">公司文档发版流程</div>
+  </z-flex>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
