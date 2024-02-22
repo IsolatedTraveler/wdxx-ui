b5f8147bb2b42eef4c721a5d813f1c869085a587
何波 <tbydmi@gmail.com>
2024-02-22 17:41:15 +0800

    Update index.vue

--- demo/src/views/gswd/view/publish/index.vue
+++ demo/src/views/gswd/view/publish/index.vue
@@ -3,10 +3,8 @@
     <div style="text-align: center;font-size: .56rem;line-height: 2;">公司文档发版流程</div>
   </z-flex>
 </template>
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
+<script lang="ts" setup>
+defineOptions({
   name: 'gswd-publish'
 })
 </script>
