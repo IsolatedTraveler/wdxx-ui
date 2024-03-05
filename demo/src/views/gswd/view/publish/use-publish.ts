5d46818ce8c52419f23f8427082bd65b416e16c1
何波 <tbydmi@gmail.com>
2024-03-05 09:32:02 +0800

    测试树形组件初始赋值

--- demo/src/views/gswd/view/publish/use-publish.ts
+++ demo/src/views/gswd/view/publish/use-publish.ts
@@ -1,13 +1,18 @@
 import { defineAsyncComponent, ref } from "vue"
 import menu from "./menu"
+import { useUserStore } from "@/store";
 export const usePublish = () => {
-  const name = ref('java'), com = import.meta.glob('./module/*.vue');
+  const userStore = useUserStore(), com = import.meta.glob('./module/*.vue')
+    // , name = ref(userStore.getTemp.gswdPublish || 'nginx');
+    , name = ref('nginx');
   function loadComponent(id: string) {
     var path = `./module/${id}.vue`, m = com[path] || com['./module/def.vue']
     return defineAsyncComponent(() => m().then((c: any) => c.default));
   }
   function changePage(data: any) {
     if (data.path) {
+      console.log(data.id)
+      userStore.setTemp({ gswdPublish: data.id })
       name.value = data.id
     }
   }
