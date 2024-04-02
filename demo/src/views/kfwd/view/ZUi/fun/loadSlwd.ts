edf57f4a7a5c65957980e82e28586f895d210e76
何波 <tbydmi@gmail.com>
2024-04-02 11:07:28 +0800

    Update loadSlwd.ts

--- demo/src/views/kfwd/view/ZUi/fun/loadSlwd.ts
+++ demo/src/views/kfwd/view/ZUi/fun/loadSlwd.ts
@@ -3,33 +3,32 @@ import { ObjAny } from "@ui/vars";
 import { defineAsyncComponent, ref } from "vue";
 interface LoadSlwdDataProp {
   title: string
-  sm:string
-  code?:string
+  sm: string
+  code?: string
 }
 export interface LoadSlwdData {
   name: string
   prop: LoadSlwdDataProp
-  load?:boolean
+  load?: boolean
 }
 // 加载示例文档
-export function loadSlwd(coms:ObjAny, val: Array<LoadSlwdData>, zjName:string, ml:string='vue/kfwd/ZUi') {
+export function loadSlwd(coms: ObjAny, val: Array<LoadSlwdData>, zjName: string, ml: string = 'vue/kfwd/ZUi') {
   const data = ref(val)
-   val = data.value
-  function loadComponent(name:string,i:number) {
-    console.log(coms, name)
+  val = data.value
+  function loadComponent(name: string, i: number) {
     const m = coms[`./${name}.vue`]
     if (m) {
       const v = val[i]
       if (!v.load) {
-        v.load= true
+        v.load = true
         localGet(`${ml}/${zjName}/${name}.text`).then((res) => {
           v.prop.code = res
         })
       }
       return defineAsyncComponent(() => m().then((c: any) => c.default))
     } else {
-      return defineAsyncComponent(()=> import('@view/gswd/view/publish/module/def.vue'))
+      return defineAsyncComponent(() => import('@view/gswd/view/publish/module/def.vue'))
     }
   }
-  return {data, loadComponent}
+  return { data, loadComponent }
 }
