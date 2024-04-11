96d48b5925713f60a0d1ae07d9980d820bdd475e
hb <tbydmi@gmail.com>
2024-04-11 22:40:23 +0800

    同步

--- packages/styles/gulpfile.ts
+++ packages/styles/gulpfile.ts
@@ -1,4 +1,4 @@
-import { epOutput } from '@ui/build-utils'
+import { epOutput } from '@ui/build-utils/var'
 import { dest, parallel, series, src } from 'gulp'
 import gulpSass from 'gulp-sass'
 import dartSass from 'sass'
@@ -21,15 +21,6 @@ function buildStylesChalk() {
   return src(path.resolve(__dirname, 'src/*.scss'))
     .pipe(sass.sync())
     .pipe(autoprefixer({ cascade: false }))
-    // .pipe(
-    //   cleanCSS({}, (details) => {
-    //     consola.success(
-    //       `${chalk.cyan(details.name)}: ${chalk.yellow(
-    //         details.stats.originalSize / 1000
-    //       )} KB -> ${chalk.green(details.stats.minifiedSize / 1000)} KB`
-    //     )
-    //   })
-    // )
     .pipe(
       rename((path) => {
         if (!noElPrefixFile.test(path.basename)) {
