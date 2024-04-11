ed7afbcb3073b0018ed3024b04720f300cd414e4
hb <tbydmi@gmail.com>
2024-04-11 20:39:02 +0800

    Update components.d.ts

--- typings/components.d.ts
+++ typings/components.d.ts
@@ -1,10 +1,10 @@
 import "@vue/runtime-core"
 declare module "@vue/runtime-core" {
   export interface GlobalComponents {
-    ZBtn: typeof import("../packages/z-ui")["ZBtn"];
-    ZBtnGroup: typeof import("../packages/z-ui")["ZBtnGroup"];
-    ZIcon: typeof import("../packages/z-ui")["ZIcon"];
-    ZForm: typeof import("../packages/z-ui")["ZForm"];
+    ZBtn: typeof import("../packages/z-uis")["ZBtn"];
+    ZBtnGroup: typeof import("../packages/z-uis")["ZBtnGroup"];
+    ZIcon: typeof import("../packages/z-uis")["ZIcon"];
+    ZForm: typeof import("../packages/z-uis")["ZForm"];
   }
   interface ComponentCustomProperties {
     
