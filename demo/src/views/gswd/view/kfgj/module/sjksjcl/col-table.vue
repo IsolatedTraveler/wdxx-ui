240926e4d4e7fe6e2b523862447d77bac313b750
hb <tbydmi@gmail.com>
2024-04-14 11:44:12 +0800

    Update col-table.vue

--- demo/src/views/gswd/view/kfgj/module/sjksjcl/col-table.vue
+++ demo/src/views/gswd/view/kfgj/module/sjksjcl/col-table.vue
@@ -13,6 +13,12 @@ const cols = [
   { id: 'col', title: '列名', fixed: true },
   { id: 'bz', title: '备注' },
   { id: 'cs', title: '测试' },
+  { id: 'cs1', title: '测试1' },
+  { id: 'cs2', title: '测试2' },
+  { id: 'cs3', title: '测试3' },
+  { id: 'cs4', title: '测试4' },
+  { id: 'cs5', title: '测试5' },
+  { id: 'cs6', title: '测试6' },
   { id: 'lx', title: '类型'}
 ],
   data = ref<any[]>([{col: 'cs',lx: 'varchar', bz: '测试'}])
