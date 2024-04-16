60e5d15b1b7b6e75a2f3034c901fc3e660d8b7c3
何波 <tbydmi@gmail.com>
2024-04-16 15:16:55 +0800

    Update def.ts

--- demo/src/views/gswd/view/publish/code/sql/deal/oracle/def.ts
+++ demo/src/views/gswd/view/publish/code/sql/deal/oracle/def.ts
@@ -2,8 +2,8 @@ import { Base64 } from "js-base64"
 
 export function def(v: string | number) {
   if (v) {
-    if (/[']/.test(v)) {
-      return `base64_de_utf8('${Base64.encode(v)}')`
+    if (/[']/.test(v + '')) {
+      return `base64_de_utf8('${Base64.encode(v + '')}')`
     } else {
       return `'${v}'`
     }
