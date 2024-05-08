2e8462ed822068db247cab0eb0b27b91e919a87d
何波 <tbydmi@gmail.com>
2024-05-08 18:36:35 +0800

    测试
    测试案例

---
+++ demo/src/views/kfwd/view/rule/data/menu.ts
@@ -0,0 +1,6 @@
+import { Menu } from "@/views/gswd/view/publish/data";
+export const menu = [
+  { id: 'vue', mc: 'vue', path: true }
+  , { id: 'commonUtil', mc: 'commonUtil', path: true }
+] as Menu[]
+export const menuDef = 'vue'
